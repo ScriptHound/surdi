a91f9992ca0c86908556f3e323352c0440ff436c
scripthound <grinkiko@gmail.com>
2022-03-21 19:04:49 +0300

    added exceptions for voice message deletion

--- main.py
+++ main.py
@@ -5,14 +5,24 @@ from dotenv import load_dotenv
 
 load_dotenv(".env")
 TOKEN = os.environ.get("ACCESS_TOKEN")
+PERMITTED_USERS = os.environ.get("LUCHSHIE_MALCHIKI")
 bot = Bot(TOKEN)
 
 
 @bot.on.message()
 async def handler(message: Message) -> str:
-    if not any(item.audio_message for item in message.attachments):
+    audio_message = None
+    if str(message.from_id) in PERMITTED_USERS:
+        return
+
+    for item in message.attachments:
+        if item.audio_message is not None:
+            audio_message = item.audio_message
+
+    if audio_message is None:
         return
     try:
+        print(audio_message.transcript)
         await bot.api.messages.delete(
             conversation_message_ids=[message.conversation_message_id],
             peer_id=message.peer_id,
